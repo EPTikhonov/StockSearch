e52f843e91ffafb9696e33b4cabca38924aa518a
EricTikhonov <28842747+EPTikhonov@users.noreply.github.com>
2018-05-15 10:52:51 -0500

    instance code simplified

--- src/StockSearch_SimpleTester.java
+++ src/StockSearch_SimpleTester.java
@@ -4,7 +4,7 @@ public class StockSearch_SimpleTester {
 
 	public static void main(String[] args) throws IOException {
 
-		StockSearch_Data application = new StockSearch_Data();
+		new StockSearch_Data();
 
 	}
 
